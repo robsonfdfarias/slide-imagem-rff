d910258d2cf3193636c49844ed7dc791c204c158
Robson Ferreira de Fairas <robsonfdfarias@gmail.com>
2024-08-02 13:28:50 -0300

    Corrigido o bug no shortcode. Havia esquecido de adicionar a classe de conexão com as tabelas.

--- inc/si-rff-shortcode.php
+++ inc/si-rff-shortcode.php
@@ -13,15 +13,18 @@
 /**
    * Includes PHP
    */
-  if(file_exists(SI_RFF_CORE_INC.'si-rff-functions.php')){
-    require_once(SI_RFF_CORE_INC.'si-rff-functions.php');
+if(file_exists(SI_RFF_CORE_INC.'si-rff-functions-class.php')){
+   require_once(SI_RFF_CORE_INC.'si-rff-functions-class.php');
 }
+
+$conection_si_rff = new SiRffConection();
 
 
 function si_rff_ex($atts){
     return "<h1>madjbndçjavçdajbvj {$atts['slideId']}</h1>";
 }
  function si_rff_shortcode_slide1($atts){
+    global $conection_si_rff;
     $html = '<p>vazio</p>';
     // Define os atributos padrão
     $atts = shortcode_atts(array(
@@ -30,9 +33,9 @@ function si_rff_ex($atts){
     ), $atts);
     //mostra os dados gravados
     if(isset($atts['slideid'])){
-        $dados = slide_image_rff_recuperar_dados_by_slide(esc_attr($atts['slideid']));
+        $dados = $conection_si_rff->slide_image_rff_recuperar_dados_by_slide(esc_attr($atts['slideid']));
     }else{
-        $dados = slide_image_rff_recuperar_dados();
+        $dados = $conection_si_rff->slide_image_rff_recuperar_dados();
     }
     
     // print_r($dados);
@@ -41,7 +44,7 @@ function si_rff_ex($atts){
             $html.= '<div style="max-width:750px !important; height: 235px; display:flex; overflow:hidden;">';
             $html.= '<div id="si_rff_imgs" style="width:750px; height: 235px; display:flex; transition: ease-in-out all 0.3s;">';
         foreach ($dados as $dado) {
-            $slideSel = slide_image_name_rff_recuperar_dados_por_ID(esc_html($dado->tableId));
+            $slideSel = $conection_si_rff->slide_image_name_rff_recuperar_dados_por_ID(esc_html($dado->tableId));
             $html .= '<div style="width:750px; height: 235px; position: relative;">
                         <div style="">
                             <img src="'.$dado->urlImg.'" class="si-rff-img-admin" style="width:750px; height: 235px">
@@ -58,6 +61,7 @@ function si_rff_ex($atts){
  }
 
  function si_rff_shortcode_slide2($atts){
+    global $conection_si_rff;
     $html = '<p>vazio</p>';
     // Define os atributos padrão
     $atts = shortcode_atts(array(
@@ -66,9 +70,9 @@ function si_rff_ex($atts){
     ), $atts);
     //mostra os dados gravados
     if(isset($atts['slideid'])){
-        $dados = slide_image_rff_recuperar_dados_by_slide(esc_attr($atts['slideid']));
+        $dados = $conection_si_rff->slide_image_rff_recuperar_dados_by_slide(esc_attr($atts['slideid']));
     }else{
-        $dados = slide_image_rff_recuperar_dados();
+        $dados = $conection_si_rff->slide_image_rff_recuperar_dados();
     }
     
     // print_r($dados);
@@ -78,7 +82,7 @@ function si_rff_ex($atts){
             $html.= '<div style="max-width:100% !important; display:flex; overflow:hidden; position:absolute; left:0;">';
             $html.= '<div id="si_rff_imgs2" style="100%; display:flex; transition: ease-in-out all 0.3s;">';
         foreach ($dados as $dado) {
-            $slideSel = slide_image_name_rff_recuperar_dados_por_ID(esc_html($dado->tableId));
+            $slideSel = $conection_si_rff->slide_image_name_rff_recuperar_dados_por_ID(esc_html($dado->tableId));
             $html .= '<div style="width:100vw; position: relative;">
                         <div style="">
                             <img src="'.$dado->urlImg.'" class="si-rff-img-admin" style="width:100vw;">
